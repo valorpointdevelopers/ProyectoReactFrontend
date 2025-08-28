40dfaee6abda90f58681f2e6f5cc4ac7d92df5d6
ANGELGARCIAfloat <21670034@itiguala.edu.mx>
2025-08-28 10:04:26 -0600

    register

--- mui-react-mui-template/src/App.tsx
+++ mui-react-mui-template/src/App.tsx
@@ -1,17 +1,20 @@
-import * as React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import DashboardLayout from './layouts/DashboardLayout'
-import Dashboard from './pages/Dashboard'
-import Users from './pages/Users'
+import * as React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import SignupForm from "./components/SignupForm"; // 👈 importar aquí
 
 export default function App() {
   return (
     <Routes>
+          <Route path="/signup" element={<SignupForm />} />
       <Route element={<DashboardLayout />}>
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
+     
       </Route>
     </Routes>
-  )
+  );
 }
