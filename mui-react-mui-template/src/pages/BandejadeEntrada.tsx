3ac2baf74becc420bad66065503530fd8d72aa2d
Omarsnzz <omar1327hackuu@gmail.com>
2025-09-18 14:50:12 -0600

    Agregue la interfas de la vista de informacion del contacto

--- mui-react-mui-template/src/pages/BandejadeEntrada.tsx
+++ mui-react-mui-template/src/pages/BandejadeEntrada.tsx
@@ -17,6 +17,8 @@ import ArticleIcon from '@mui/icons-material/Article';
 import { io, Socket } from "socket.io-client";
 
 import welcomeCats from "../images/no-chat-found.svg";
+// Asegúrate de que tu tipo 'Chat' incluya la propiedad opcional 'profilePicUrl'
+// export interface Chat { ...; profilePicUrl?: string; }
 import { Chat, Message } from '../types';
 import config from "../config.json";
 import { db } from '../db';
@@ -507,7 +509,7 @@ const BandejadeEntrada: React.FC = () => {
                 display: "flex", 
                 flexDirection: "column", 
                 height: "100%",
-                boxSizing: 'border-box'
+                boxSizing: 'border-box' // <-- AQUÍ LA CORRECCIÓN
             }}>
                 <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2}}>
                     <Typography variant="h5">Chats</Typography>
